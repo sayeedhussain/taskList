9169bfcc683b80b6efd4c2d08bf2cc13a4203794
Sayeed Munawar Hussain <sayeedhussain19@gmail.com>
2024-07-17 20:33:24 -0500

    Update

--- src/main/java/org/example/Tasks/Model/TaskList.java
+++ src/main/java/org/example/Tasks/Model/TaskList.java
@@ -32,4 +32,15 @@ public class TaskList {
         return tasks.stream().filter(it -> it.isDueToday()).toList();
     }
 
+    public List<Task> sortTasks(SortOrder sortOrder) {
+        List<Task> sortedTaskList = new ArrayList<>(tasks);
+        Comparator comparator = getComparator(sortOrder);
+        sortedTaskList.sort(comparator);
+        return sortedTaskList;
+    }
+
+    private Comparator getComparator(SortOrder sortOrder) {
+        return sortOrder == SortOrder.CREATED_DATE
+                ? new TaskComparators.DueDateComparator() : new TaskComparators.CreatedDateComparator();
+    }
 }
